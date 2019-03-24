d299362357bebaf9c0611e1652218bf3af64b701
yaozong <hpps9603@gmail.com>
2019-03-24 23:48:56 +0800

    add button_init and detect the button then blink

--- blink.c
+++ blink.c
@@ -26,6 +26,23 @@ void led_init(unsigned int led)
 	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(led));
 }
 
+void button_init()
+{
+	SET_BIT(RCC_BASE + RCC_AHB1ENR_OFFSET, GPIO_EN_BIT(GPIO_PORTA));
+
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_MODER_OFFSET, MODERy_1_BIT(USER_BUTTON));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_MODER_OFFSET, MODERy_0_BIT(USER_BUTTON));
+
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_OTYPER_OFFSET, OTy_BIT(USER_BUTTON));
+	
+	SET_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(USER_BUTTON));
+	SET_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(USER_BUTTON));
+	
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(USER_BUTTON));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(USER_BUTTON));
+	
+}
+
 /**
  * 
  * blink LED forever
@@ -34,11 +51,16 @@ void led_init(unsigned int led)
 void blink(unsigned int led)
 {
 	led_init(led);
-
+	button_init();
+	
 	unsigned int i;
 
 	while (1)
 	{
+		if(READ_BIT( GPIO_BASE(GPIO_PORTA) + GPIOx_IDR_OFFSET , USER_BUTTON ) == UINT32_1)
+		{
+		while (1)
+		{
 		//set GPIOD led pin
 		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT(led));
 
@@ -50,32 +72,8 @@ void blink(unsigned int led)
 
 		for (i = 0; i < 100000; i++)
 			;
+		}
+		}
 	}
 }
 
-/**
- * 
- * blink LED x count
- * 
- */
-void blink_count(unsigned int led, unsigned int count)
-{
-	led_init(led);
-
-	unsigned int i;
-
-	while (count--)
-	{
-		//set GPIOD led pin
-		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT(led));
-
-		for (i = 0; i < 100000; i++)
-			;
-
-		//reset GPIOD led pin
-		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT(led));
-
-		for (i = 0; i < 100000; i++)
-			;
-	}
-}
